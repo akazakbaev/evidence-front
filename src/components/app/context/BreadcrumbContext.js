a25e2ece74850fd066dade48096f6bf18c87f717
Азим Казакбаев <akazakbaev@srs.lan>
2022-01-16 15:59:03 +0600

    breadcrumb

--- src/components/app/context/BreadcrumbContext.js
+++ src/components/app/context/BreadcrumbContext.js
@@ -8,12 +8,12 @@ const BreadcrumbStateContext = React.createContext({
 
 
 function BreadcrumbProvider({children}) {
-    const [state, dispatch] = React.useReducer(countReducer, {count: 0})
-    return (
-        <BreadcrumbStateContext.Provider value={state}>
-            {children}
-        </BreadcrumbStateContext.Provider>
-    )
+    // const [state, dispatch] = React.useReducer(countReducer, {count: 0})
+    // return (
+    //     <BreadcrumbStateContext.Provider value={state}>
+    //         {children}
+    //     </BreadcrumbStateContext.Provider>
+    // )
 }
 
 
